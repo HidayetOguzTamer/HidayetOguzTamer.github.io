faa92449bc851978e06487e624696d31f2eb547f
Berke-aras <berkearastaman@outlook.com>
2024-05-21 00:06:37 +0300

    update firebase

--- backend/admin-giris.js
+++ backend/admin-giris.js
@@ -1,14 +1,12 @@
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCvWi8tDBKS-Y5vV3BVDz8NrIeILG23Sg0",
-    authDomain: "aricilik-90bc8.firebaseapp.com",
-    databaseURL:
-        "https://aricilik-90bc8-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "aricilik-90bc8",
-    storageBucket: "aricilik-90bc8.appspot.com",
-    messagingSenderId: "799487412043",
-    appId: "1:799487412043:web:c403e862bd919894014ace",
-    measurementId: "G-0E67V7HFZY",
+    apiKey: "AIzaSyDmUHmAutU8W1kMXMcfqRVMOazTs9SNFcs",
+    authDomain: "harummmw.firebaseapp.com",
+    projectId: "harummmw",
+    storageBucket: "harummmw.appspot.com",
+    messagingSenderId: "686002002322",
+    appId: "1:686002002322:web:7b82db1a76251273fd894b",
+    measurementId: "G-JTCGPWHFTM",
 };
 firebase.initializeApp(firebaseConfig);
 
